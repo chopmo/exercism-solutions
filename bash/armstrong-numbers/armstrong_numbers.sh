fd80f0295a484a9e400f50b603dbc4880853d542
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2019-12-25 20:21:26 +0100

    Added comment about how I could have use declare

--- bash/armstrong-numbers/armstrong_numbers.sh
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -2,6 +2,10 @@
 
 num_digits=${#1}
 
+# I later learned that by using
+# `declare -i sum`, I could have summed up with:
+# sum+=$(( ${1:$i:1} ** $num_digits ))
+
 for (( i=0; i<$num_digits; i++ )); do
   sum=$(( $sum + (${1:$i:1} ** $num_digits) ))
 done
