2e4dc55bba5df977c8a3290ac8afc7e3a9b7c879
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2019-12-29 20:38:12 +0100

    Slight refactoring

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,17 +1,16 @@
-(ns run-length-encoding
-  (:require [clojure.string :as str]))
+(ns run-length-encoding)
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
   [plain-text]
-  (loop [s   (seq plain-text)
+  (loop [s   plain-text
          cnt 1
-         res ()]
+         res ""]
     (let [c    (first s)
           next (second s)]
       (cond
         (nil? c)
-        (apply str res)
+        res
 
         (= c next)
         (recur (rest s)
@@ -21,23 +20,27 @@
         :else
         (recur (rest s)
                1
-               (concat res [(when (> cnt 1) cnt) c]))))))
+               (apply str
+                      res
+                      (when (> cnt 1) cnt)
+                      (str c)))))))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [cipher-text]
   (loop [tokens (re-seq #"\d+|[^\d]" cipher-text)
-         res ()]
-    (let [[t1 t2] (take 2 tokens)
-          count (try (Integer/parseInt t1) (catch Exception _ nil))]
+         res    ""]
+    (let [count (try
+                  (Integer/parseInt (first tokens))
+                  (catch Exception _ nil))]
       (cond
-        (nil? t1)
-        (apply str res)
+        (empty? tokens)
+        res
 
         count
         (recur (drop 2 tokens)
-               (concat res (repeat count t2)))
+               (apply str res (repeat count (second tokens))))
 
         :else
         (recur (rest tokens)
-               (concat res [t1]))))))
+               (str res (first tokens)))))))
