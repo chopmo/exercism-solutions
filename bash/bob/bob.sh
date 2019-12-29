15e108d224fb9e3bc494457385d1c314762687a5
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2019-12-29 11:47:34 +0100

    Refactored (with comment)

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
+# I tried to use $(expr ...) to assign the comparisons to variables,
+# but without luck. Looked like I needed quoting, but I gave up and
+# went with a simpler refactoring instead.
+
 if [[ ${1^^} == $1 && ${1^^} =~ [A-Z] ]]; then
-  if [[ $1 =~ .*\?$ ]]; then
-    echo "Calm down, I know what I'm doing!"
-  else
-    echo "Whoa, chill out!"
-  fi
-elif [[ $1 =~ .*\?[[:blank:]]*$ ]]; then
+  shouting=true
+fi
+
+if [[ $1 =~ .*\?[[:blank:]]*$ ]]; then
+  question=true
+fi
+
+if [[ $1 =~ ^[$'\n'$'\r'[:blank:]]*$ ]]; then
+  silence=true
+fi
+
+if [[ $shouting && $question ]]; then
+  echo "Calm down, I know what I'm doing!"
+elif [[ $shouting ]]; then
+  echo "Whoa, chill out!"
+elif [[ $question ]]; then
   echo "Sure."
-elif [[ $1 =~ ^[$'\n'$'\r'[:blank:]]*$ ]]; then
+elif [[ $silence ]]; then
   echo "Fine. Be that way!"
 else
   echo "Whatever."
