0180eed47718ba223ccf9cd5ae2ced24128e90db
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2019-12-30 15:22:51 +0100

    Leap years in bash

---
+++ bash/leap/leap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ $# != 1 || $1 =~ [^0-9] ]]; then
+  echo "Usage: leap.sh <year>"
+  exit 1
+fi
+
+if (( $1 % 400 == 0 ||
+      ($1 % 4 == 0 && $1 % 100 != 0) )); then
+  echo "true"
+else
+  echo "false"
+fi
